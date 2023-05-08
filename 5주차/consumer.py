44460b2e4069cfe410d1e2a94e6f1409b980846a
swacademy <blueexpert@hotmail.com>
2023-05-08 12:46:34 +0900

    5주차 실습파일

---
+++ 5주차/consumer.py
@@ -0,0 +1,18 @@
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer(
+	'henry-topic2',
+	bootstrap_servers = [
+		'b-1.henrymskcluster.jm4797.c4.kafka.ap-northeast-2.amazonaws.com:9092',
+        'b-2.henrymskcluster.jm4797.c4.kafka.ap-northeast-2.amazonaws.com:9092'
+	],
+	auto_offset_reset='latest',
+	group_id='mygroup',
+	enable_auto_commit=True,
+	value_deserializer=lambda x : loads(x).decode('utf-8'),
+	consumer_timeout_ms=10000
+)
+
+for msg in consumer:
+	print(f'topic={msg.topic}, partition={msg.partition}, offset={msg.offset}, key={msg.key}, value={msg.value}')
